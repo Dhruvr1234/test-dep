9293a369aec56cac3fb82f110c012c52b79ab07a
Dhruv Patel <dhtuvrp@gmail.com>
2025-01-17 15:09:48 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/',(req,res)=>{
+    res.send({
+        message : `Server is running on PORT ${PORT}`
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is started on new server ${PORT}`);
+})
